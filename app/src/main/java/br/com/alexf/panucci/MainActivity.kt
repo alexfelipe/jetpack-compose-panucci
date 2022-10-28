d7b690dcfcb08b8d940e270972152da60dbcaccc
Alex Felipe <alexfelipevieira@gmail.com>
2022-10-28 20:37:29 -0300

    initial project

---
+++ app/src/main/java/br/com/alexf/panucci/MainActivity.kt
@@ -0,0 +1,158 @@
+package br.com.alexf.panucci
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AutoAwesome
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.RestaurantMenu
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.LocalBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.alexf.panucci.ui.screens.HomeScreen
+import br.com.alexf.panucci.ui.theme.PanucciTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PanucciTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    App {
+                        HomeScreen()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun App(content: @Composable () -> Unit) {
+    var selectedItem by remember {
+        mutableStateOf(0)
+    }
+    val items = listOf(
+        "Destaques" to Icons.Filled.AutoAwesome,
+        "Menu" to Icons.Filled.RestaurantMenu,
+        "Bebidas" to Icons.Outlined.LocalBar
+    )
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                Spacer(Modifier.height(12.dp))
+                items.forEach { item ->
+                    val name = item.first
+                    NavigationDrawerItem(
+                        icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                        label = { Text(name) },
+                        selected = items.indexOfFirst { pair ->
+                            pair.first == name
+                        } == selectedItem,
+                        onClick = {
+                            scope.launch { drawerState.close() }
+                            selectedItem = items.indexOfFirst { pair ->
+                                pair.first == name
+                            }
+                        },
+                        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                    )
+                }
+            }
+        },
+        drawerState = drawerState,
+    ) {
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(title = {
+                    Text(text = "Restorante Panucci")
+                }, navigationIcon = {
+                    IconButton(onClick = {
+                        scope.launch {
+                            drawerState.open()
+                        }
+                    }) {
+                        Icon(Icons.Filled.Menu, contentDescription = null)
+                    }
+                }, actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Outlined.AccountCircle, null)
+                    }
+                })
+            },
+            bottomBar = {
+                NavigationBar {
+                    items.forEachIndexed { index, item ->
+                        val label = item.first
+                        val icon = item.second
+                        NavigationBarItem(
+                            icon = { Icon(icon, contentDescription = label) },
+                            label = { Text(label) },
+                            selected = selectedItem == index,
+                            onClick = { selectedItem = index }
+                        )
+                    }
+                }
+            },
+        ) {
+            Box(
+                modifier = Modifier.padding(it)
+            ) {
+                content()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppPreview() {
+    PanucciTheme {
+        Surface {
+            App {}
+        }
+    }
+}
---
+++ app/src/main/java/br/com/alexf/panucci/ui/screens/HomeScreen.kt
@@ -0,0 +1,21 @@
+package br.com.alexf.panucci.ui.screens
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.alexf.panucci.ui.theme.PanucciTheme
+
+@Composable
+fun HomeScreen() {
+
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    PanucciTheme {
+        Surface {
+            HomeScreen()
+        }
+    }
+}
