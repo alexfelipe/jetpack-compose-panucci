4ec02632f56b4ba5a21988ea6e6b07abcc6fde2c
Alex Felipe <alexfelipevieira@gmail.com>
2022-12-20 10:28:47 -0300

    4.1

--- app/src/main/java/br/com/alura/panucci/ui/viewmodels/ProductDetailsViewModel.kt
+++ app/src/main/java/br/com/alura/panucci/ui/viewmodels/ProductDetailsViewModel.kt
@@ -1,12 +1,14 @@
 package br.com.alura.panucci.ui.viewmodels
 
-import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import br.com.alura.panucci.dao.ProductDao
 import br.com.alura.panucci.ui.uistate.ProductDetailsUiState
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class ProductDetailsViewModel(
     private val dao: ProductDao = ProductDao()
@@ -16,9 +18,12 @@ class ProductDetailsViewModel(
     val uiState = _uiState.asStateFlow()
 
     fun findProductById(id: String) {
-        dao.findById(id)?.let { product ->
-            _uiState.update {
-                it.copy(product = product)
+        viewModelScope.launch {
+            delay(5000)
+            dao.findById(id)?.let { product ->
+                _uiState.update {
+                    it.copy(product = product)
+                }
             }
         }
     }
